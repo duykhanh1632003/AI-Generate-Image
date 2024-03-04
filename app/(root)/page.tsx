0f91cd542b5c2a9bd2a6d14d16461a086cb4942c
Nguyễn Duy Khánh <109708814+duykhanh1632003@users.noreply.github.com>
2024-03-04 20:56:17 +0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+
+const Home = () => {
+  return (
+    <section className="home">
+      <p>Home</p>
+    </section>
+  );
+};
+
+export default Home;
